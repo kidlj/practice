889dc47a7ab5d1fb3d8f0e9e02a7913f4959959c
Jian Li <kdlijian@gmail.com>
2019-09-14 14:25:21 +0800

    update repo

---
+++ crawler/breadthfirst/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/kidlj/demo/crawler/links"
+)
+
+// CONCURRENCY sets the concurrency level
+const CONCURRENCY = 40
+
+func main() {
+	worklist := make(chan []string)
+	unseenlinks := make(chan string)
+	contentsAll := [][]byte{}
+
+	go func() {
+		worklist <- os.Args[1:]
+	}()
+
+	for i := 0; i < CONCURRENCY; i++ {
+		go func() {
+			for link := range unseenlinks {
+				foundLinks, contents := crawl(link)
+				contentsAll = append(contentsAll, contents)
+				go func() {
+					worklist <- foundLinks
+				}()
+			}
+		}()
+	}
+
+	seen := make(map[string]bool)
+	for list := range worklist {
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				unseenlinks <- link
+			}
+		}
+	}
+}
+
+func crawl(url string) ([]string, []byte) {
+	fmt.Println(url)
+	list, contents, err := links.Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return list, contents
+}
--- json/types.go
+++ json/types.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"os"
 )
 
 type User struct {
@@ -17,14 +16,135 @@ type Profile struct {
 	Email string `json:"email,omitempty"`
 }
 
+type TrafficShapingPolicy struct {
+	// If traffic shaping policy enabled for this backend, indicating whether it's a default backend or not
+	Enabled bool `json:"enabled"`
+	// Policy of traffic shaping
+	Policy string `json:"policy"`
+	// Order is the evaluation order when matching traffic policy
+	Order int `json:"order"`
+	// Weight (0-100) of traffic to redirect to the backend.
+	// e.g. Weight 20 means 20% of traffic will be redirected to the backend and 80% will remain
+	// with the other backend. 0 weight will not send any traffic to this backend
+	Weight int `json:"weight"`
+	// Match conditions
+	Match [][]Match `json:"match"`
+}
+
+type Match struct {
+	Header Header `json:"header"`
+	Cookie Cookie `json:"cookie"`
+}
+
+type Header struct {
+	// Header on which to redirect requests to this backend
+	Header string `json:"header"`
+	// HeaderValue on which to redirect requests to this backend
+	HeaderValue string `json:"headerValue"`
+}
+
+type Cookie struct {
+	// Cookie on which to redirect requests to this backend
+	Cookie string `json:"cookie"`
+	// CookieValue on which to redirect requests to this backend
+	CookieValue string `json:"cookieValue"`
+}
+
 // test json omitempty
 func main() {
-	user := User{
-		ID: "test",
+	// user := User{
+	// 	ID: "test",
+	// }
+	// bytes, err := json.Marshal(user)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// t := TrafficShapingPolicy{
+	// 	Match: [][]Match{
+	// 		{
+	// 			{
+	// 				Header: Header{
+	// 					Header:      "mellon",
+	// 					HeaderValue: "coolie",
+	// 				},
+	// 			},
+	// 			{
+	// 				Cookie: Cookie{
+	// 					Cookie:      "hello",
+	// 					CookieValue: "world",
+	// 				},
+	// 			},
+	// 			{
+	// 				Header: Header{
+	// 					Header:      "music",
+	// 					HeaderValue: "post-punk",
+	// 				},
+	// 			},
+	// 		},
+	// 		{
+	// 			{
+	// 				Header: Header{
+	// 					Header:      "x-version",
+	// 					HeaderValue: "feature",
+	// 				},
+	// 			},
+	// 			{
+	// 				Cookie: Cookie{
+	// 					Cookie:      "y-cookie",
+	// 					CookieValue: "canary",
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// }
+
+	t1 := [][]Match{
+		{
+			{
+				Header: Header{
+					Header:      "mellon",
+					HeaderValue: "coolie",
+				},
+			},
+			{
+				Cookie: Cookie{
+					Cookie:      "hello",
+					CookieValue: "world",
+				},
+			},
+			{
+				Header: Header{
+					Header:      "music",
+					HeaderValue: "post-punk",
+				},
+			},
+		},
+		{
+			{
+				Header: Header{
+					Header:      "x-version",
+					HeaderValue: "feature",
+				},
+			},
+			{
+				Cookie: Cookie{
+					Cookie:      "y-cookie",
+					CookieValue: "canary",
+				},
+			},
+		},
 	}
-	bytes, err := json.Marshal(user)
+
+	bytes, err := json.Marshal(t1)
 	if err != nil {
 		fmt.Println(err)
 	}
-	os.Stdout.Write(bytes)
+	fmt.Println(string(bytes))
+	t2 := [][]Match{}
+	// t2 := TrafficShapingPolicy{}
+	if err := json.Unmarshal(bytes, &t2); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%#v\n", t2)
 }
