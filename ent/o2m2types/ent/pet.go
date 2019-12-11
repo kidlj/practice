a2ed3db4dbbd8aefaf078ccbc73b261173892bb3
Jian Li <kdlijian@gmail.com>
2019-12-11 12:01:30 +0800

    ent o2m2types demo done

---
+++ ent/o2m2types/ent/pet.go
@@ -0,0 +1,96 @@
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+// Code generated (@generated) by entc, DO NOT EDIT.
+
+package ent
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/facebookincubator/ent/dialect/sql"
+)
+
+// Pet is the model entity for the Pet schema.
+type Pet struct {
+	config `json:"-"`
+	// ID of the ent.
+	ID int `json:"id,omitempty"`
+	// Name holds the value of the "name" field.
+	Name string `json:"name,omitempty"`
+}
+
+// FromRows scans the sql response data into Pet.
+func (pe *Pet) FromRows(rows *sql.Rows) error {
+	var scanpe struct {
+		ID   int
+		Name sql.NullString
+	}
+	// the order here should be the same as in the `pet.Columns`.
+	if err := rows.Scan(
+		&scanpe.ID,
+		&scanpe.Name,
+	); err != nil {
+		return err
+	}
+	pe.ID = scanpe.ID
+	pe.Name = scanpe.Name.String
+	return nil
+}
+
+// QueryOwner queries the owner edge of the Pet.
+func (pe *Pet) QueryOwner() *UserQuery {
+	return (&PetClient{pe.config}).QueryOwner(pe)
+}
+
+// Update returns a builder for updating this Pet.
+// Note that, you need to call Pet.Unwrap() before calling this method, if this Pet
+// was returned from a transaction, and the transaction was committed or rolled back.
+func (pe *Pet) Update() *PetUpdateOne {
+	return (&PetClient{pe.config}).UpdateOne(pe)
+}
+
+// Unwrap unwraps the entity that was returned from a transaction after it was closed,
+// so that all next queries will be executed through the driver which created the transaction.
+func (pe *Pet) Unwrap() *Pet {
+	tx, ok := pe.config.driver.(*txDriver)
+	if !ok {
+		panic("ent: Pet is not a transactional entity")
+	}
+	pe.config.driver = tx.drv
+	return pe
+}
+
+// String implements the fmt.Stringer.
+func (pe *Pet) String() string {
+	var builder strings.Builder
+	builder.WriteString("Pet(")
+	builder.WriteString(fmt.Sprintf("id=%v", pe.ID))
+	builder.WriteString(", name=")
+	builder.WriteString(pe.Name)
+	builder.WriteByte(')')
+	return builder.String()
+}
+
+// Pets is a parsable slice of Pet.
+type Pets []*Pet
+
+// FromRows scans the sql response data into Pets.
+func (pe *Pets) FromRows(rows *sql.Rows) error {
+	for rows.Next() {
+		scanpe := &Pet{}
+		if err := scanpe.FromRows(rows); err != nil {
+			return err
+		}
+		*pe = append(*pe, scanpe)
+	}
+	return nil
+}
+
+func (pe Pets) config(cfg config) {
+	for _i := range pe {
+		pe[_i].config = cfg
+	}
+}
---
+++ ent/o2m2types/ent/pet_create.go
@@ -0,0 +1,108 @@
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+// Code generated (@generated) by entc, DO NOT EDIT.
+
+package ent
+
+import (
+	"context"
+	"errors"
+
+	"github.com/facebookincubator/ent/dialect/sql"
+	"github.com/kidlj/demo/ent/o2m2types/ent/pet"
+)
+
+// PetCreate is the builder for creating a Pet entity.
+type PetCreate struct {
+	config
+	name  *string
+	owner map[int]struct{}
+}
+
+// SetName sets the name field.
+func (pc *PetCreate) SetName(s string) *PetCreate {
+	pc.name = &s
+	return pc
+}
+
+// SetOwnerID sets the owner edge to User by id.
+func (pc *PetCreate) SetOwnerID(id int) *PetCreate {
+	if pc.owner == nil {
+		pc.owner = make(map[int]struct{})
+	}
+	pc.owner[id] = struct{}{}
+	return pc
+}
+
+// SetNillableOwnerID sets the owner edge to User by id if the given value is not nil.
+func (pc *PetCreate) SetNillableOwnerID(id *int) *PetCreate {
+	if id != nil {
+		pc = pc.SetOwnerID(*id)
+	}
+	return pc
+}
+
+// SetOwner sets the owner edge to User.
+func (pc *PetCreate) SetOwner(u *User) *PetCreate {
+	return pc.SetOwnerID(u.ID)
+}
+
+// Save creates the Pet in the database.
+func (pc *PetCreate) Save(ctx context.Context) (*Pet, error) {
+	if pc.name == nil {
+		return nil, errors.New("ent: missing required field \"name\"")
+	}
+	if len(pc.owner) > 1 {
+		return nil, errors.New("ent: multiple assignments on a unique edge \"owner\"")
+	}
+	return pc.sqlSave(ctx)
+}
+
+// SaveX calls Save and panics if Save returns an error.
+func (pc *PetCreate) SaveX(ctx context.Context) *Pet {
+	v, err := pc.Save(ctx)
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func (pc *PetCreate) sqlSave(ctx context.Context) (*Pet, error) {
+	var (
+		res     sql.Result
+		builder = sql.Dialect(pc.driver.Dialect())
+		pe      = &Pet{config: pc.config}
+	)
+	tx, err := pc.driver.Tx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	insert := builder.Insert(pet.Table).Default()
+	if value := pc.name; value != nil {
+		insert.Set(pet.FieldName, *value)
+		pe.Name = *value
+	}
+
+	id, err := insertLastID(ctx, tx, insert.Returning(pet.FieldID))
+	if err != nil {
+		return nil, rollback(tx, err)
+	}
+	pe.ID = int(id)
+	if len(pc.owner) > 0 {
+		for eid := range pc.owner {
+			query, args := builder.Update(pet.OwnerTable).
+				Set(pet.OwnerColumn, eid).
+				Where(sql.EQ(pet.FieldID, id)).
+				Query()
+			if err := tx.Exec(ctx, query, args, &res); err != nil {
+				return nil, rollback(tx, err)
+			}
+		}
+	}
+	if err := tx.Commit(); err != nil {
+		return nil, err
+	}
+	return pe, nil
+}
