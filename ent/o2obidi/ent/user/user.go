df149a5c493efda36d854c9fc47b581db0faefe3
Jian Li <kdlijian@gmail.com>
2019-12-12 23:14:27 +0800

    ent o2obidi example done

---
+++ ent/o2obidi/ent/user/user.go
@@ -0,0 +1,32 @@
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+// Code generated (@generated) by entc, DO NOT EDIT.
+
+package user
+
+const (
+	// Label holds the string label denoting the user type in the database.
+	Label = "user"
+	// FieldID holds the string denoting the id field in the database.
+	FieldID = "id"
+	// FieldAge holds the string denoting the age vertex property in the database.
+	FieldAge = "age"
+	// FieldName holds the string denoting the name vertex property in the database.
+	FieldName = "name"
+
+	// Table holds the table name of the user in the database.
+	Table = "users"
+	// SpouseTable is the table the holds the spouse relation/edge.
+	SpouseTable = "users"
+	// SpouseColumn is the table column denoting the spouse relation/edge.
+	SpouseColumn = "user_spouse_id"
+)
+
+// Columns holds all SQL columns are user fields.
+var Columns = []string{
+	FieldID,
+	FieldAge,
+	FieldName,
+}
