d822df213d858ea32679984062582b96f597e778
Jian Li <kdlijian@gmail.com>
2020-03-21 23:49:03 +0800

    Ex8.4 done

---
+++ gopl.io/ch8/reverb2/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8000")
+	log.Println("Listening on :8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	var wg sync.WaitGroup
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		wg.Add(1)
+		go echo(&wg, c, input.Text(), 3*time.Second)
+	}
+	wg.Wait()
+	// TODO: ignoring potential errors from input.Err()
+	c.Close()
+}
+
+func echo(wg *sync.WaitGroup, c net.Conn, shout string, delay time.Duration) {
+	defer wg.Done()
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
